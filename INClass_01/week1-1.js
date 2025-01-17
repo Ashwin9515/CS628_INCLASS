358d410649816541abb2b2e15f5f314a949c80f9
Ashwin Shastry Paturi <156279205+Ashwin9515@users.noreply.github.com>
2025-01-17 02:15:57 +0000

    INClass01 - Activity

---
+++ INClass_01/week1-1.js
@@ -0,0 +1,12 @@
+console.log('Hello World!')
+
+a = 1
+b = 1
+console.log(a+b)
+
+if(a > b)
+    console.log('a more than b')
+else
+    console.log('a not more than b')
+for(i = 0; i < 10; i++)
+    console.log(i);
